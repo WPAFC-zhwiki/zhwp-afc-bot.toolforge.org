99515147a11016c8431088eafd44d321111a44d3
sunafterrainwm <sunafterrainwm@gmail.com>
2024-04-05 16:44:39 +0800

    Update move-namespace-error.ts

--- src/shortcut/move-namespace-error.ts
+++ src/shortcut/move-namespace-error.ts
@@ -11,7 +11,8 @@ export function onRequest( req: express.Request, res: express.Response ) {
 			const moveQuery = new URLSearchParams( {
 				wpOldTitle: title,
 				wpNewTitle: `Draft:${ title }`,
-				wpReason: '由[[Wikipedia:建立條目|建立條目精靈]]建立但錯誤放置在主名字空間且未符合條目收錄要求的草稿'
+				wpReason: '由[[Wikipedia:建立條目|建立條目精靈]]建立但錯誤放置在主名字空間且未符合條目收錄要求的草稿',
+				wpLeaveRedirect: '0'
 			} );
 			return movedPermanently( `https://zh.wikipedia.org/wiki/Special:MovePage?${ moveQuery.toString() }`, req, res );
 		}
